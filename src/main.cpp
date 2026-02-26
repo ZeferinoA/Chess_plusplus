aa0644123d8789acaeeb6b09077aa703db989e16
Zenas Ortega <ortegaze@flip3.engr.oregonstate.edu>
2026-02-26 11:14:43 -0800

    added tutorial files and game mode selection

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include "Board.hpp"
+#include "Tutorial.hpp"
 #include <iostream>
 
 int main() {
+    int game_mode;
+    std::cout << "Choose Mode " << std::endl;
+    std::cout << "1. Tutorial \n2. Regular game \n3. Crazy mode" << std::endl;
+    std::cin >> game_mode;
+    if(game_mode == 1){
+        //call the tutorial file fxns
+    }else if(game_mode == 2){
+
+    }else{
+
+    }
+    
     Board board;
     int turn = 0;
 
